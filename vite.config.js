2ce6f02475bc01540315634e19ba7059f0d7832f
Muhammad Sarim <sarimkhan13579@gmail.com>
2025-07-21 06:55:12 -0700

    refactor: streamline Vite configuration by removing unused options and consolidating build settings

--- vite.config.js
+++ vite.config.js
@@ -2,45 +2,23 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [
     tailwindcss(),
     react()
   ],
   build: {
+    outDir: 'dist',
+    sourcemap: false,
+    minify: 'esbuild',
     rollupOptions: {
       output: {
         manualChunks: {
           vendor: ['react', 'react-dom'],
-          router: ['react-router-dom'],
-          icons: ['react-icons'],
-          axios: ['axios']
-        },
-        assetFileNames: (assetInfo) => {
-          const info = assetInfo.name.split('.');
-          const extType = info[info.length - 1];
-          if (/png|jpe?g|svg|gif|tiff|bmp|ico/i.test(extType)) {
-            return `img/[name]-[hash][extname]`;
-          }
-          return `assets/[name]-[hash][extname]`;
+          router: ['react-router-dom']
         }
       }
-    },
-    chunkSizeWarningLimit: 1000,
-    minify: 'terser',
-    terserOptions: {
-      compress: {
-        drop_console: true,
-        drop_debugger: true
-      }
-    }
-  },
-  server: {
-    hmr: {
-      overlay: false
     }
   }
 })
-
 
